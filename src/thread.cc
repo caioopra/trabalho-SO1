7e57e3df5a32b27862308ee24f285e9975321676
fillipimangrich <costa.mangrich@gmail.com>
2023-03-28 22:18:32 -0300

    add thread_count,implement switch context and exit

--- src/thread.cc
+++ src/thread.cc
@@ -8,12 +8,20 @@
 
 __BEGIN_API
 
+unsigned int Thread::thread_count = 0;
+
+
 template <typename... Tn>
 static int Thread::switch_context(Thread* prev, Thread* next) {
+    Thread::_running = next;
+    return CPU::switch_context(prev->context(),next->contex();
 }
 
 template <typename... Tn>
 void Thread::thread_exit(int exit_code) {
+    delete this->_context;
+    
+    Thread::thread_count--;
 }
 
 template <typename... Tn>
